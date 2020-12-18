63ccac7e2f7b723de720242fc7210dc6bdbb2519
Woszuk <Woszuk@users.noreply.github.com>
2020-12-18 20:26:34 +0100

    quantity of bags

--- src/main/java/pl/coderslab/charity/controller/HomeController.java
+++ src/main/java/pl/coderslab/charity/controller/HomeController.java
@@ -3,20 +3,24 @@ package pl.coderslab.charity.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.service.DonationServiceImpl;
 import pl.coderslab.charity.service.InstitutionServiceImpl;
 
 @Controller
 public class HomeController {
 
     private InstitutionServiceImpl institutionService;
+    private DonationServiceImpl donationService;
 
-    public HomeController(InstitutionServiceImpl institutionService) {
+    public HomeController(InstitutionServiceImpl institutionService, DonationServiceImpl donationService) {
         this.institutionService = institutionService;
+        this.donationService = donationService;
     }
 
     @GetMapping("/")
     public String homeAction(Model model){
         model.addAttribute("institutions", institutionService.allInstitutions());
+        model.addAttribute("quantityOfBags", donationService.allBags());
         return "index";
     }
 }
