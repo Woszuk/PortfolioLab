49315725466d71af646ac5b5fa07aeff3896b591
Woszuk <Woszuk@users.noreply.github.com>
2020-12-18 22:13:59 +0100

    complementing html files and controller

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -1,4 +1,25 @@
 package pl.coderslab.charity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.coderslab.charity.service.CategoryServiceImpl;
+import pl.coderslab.charity.service.InstitutionServiceImpl;
+
+@Controller
 public class DonationController {
+    private InstitutionServiceImpl institutionService;
+    private CategoryServiceImpl categoryService;
+
+    public DonationController(InstitutionServiceImpl institutionService, CategoryServiceImpl categoryService) {
+        this.institutionService = institutionService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/donation")
+    private String donation(Model model) {
+        model.addAttribute("categories", categoryService.allCategory());
+        model.addAttribute("institutions", institutionService.allInstitutions());
+        return "form";
+    }
 }
