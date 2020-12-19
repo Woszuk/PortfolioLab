73da293054a8ca98f6f356c6ee8e8df2187f79e0
Woszuk <Woszuk@users.noreply.github.com>
2020-12-19 20:41:40 +0100

    displaying institutions and downloading institutions from the form has been fixed

--- src/main/java/pl/coderslab/charity/controller/DonationController.java
+++ src/main/java/pl/coderslab/charity/controller/DonationController.java
@@ -2,9 +2,15 @@ package pl.coderslab.charity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.service.CategoryServiceImpl;
 import pl.coderslab.charity.service.InstitutionServiceImpl;
+
+import javax.validation.Valid;
 
 @Controller
 public class DonationController {
@@ -16,10 +22,19 @@ public class DonationController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/donation")
+    @GetMapping("/form")
     private String donation(Model model) {
+        model.addAttribute("donation", new Donation());
         model.addAttribute("categories", categoryService.allCategory());
         model.addAttribute("institutions", institutionService.allInstitutions());
         return "form";
+    }
+
+    @PostMapping("/form")
+    private String donationForm(@ModelAttribute("donation") @Valid Donation donation, BindingResult result){
+        if(result.hasErrors()){
+            return "form";
+        }
+        return "form-confirmation";
     }
 }
